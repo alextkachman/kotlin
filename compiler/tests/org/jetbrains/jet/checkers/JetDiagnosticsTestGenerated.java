90282d95dc7d92642dd164910bed3a89564f2a4c
svtk <svetlana.isakova@jetbrains.com>
2013-11-19 23:03:52 +0400

    Fixed inference in a simple case.
    Try number lower bounds before upper bounds when computing a value.

--- compiler/tests/org/jetbrains/jet/checkers/JetDiagnosticsTestGenerated.java
+++ compiler/tests/org/jetbrains/jet/checkers/JetDiagnosticsTestGenerated.java
@@ -3232,6 +3232,11 @@ public class JetDiagnosticsTestGenerated extends AbstractDiagnosticsTestWithEage
                 doTest("compiler/testData/diagnostics/tests/inference/possibleCycleOnConstraints.kt");
             }
             
+            @TestMetadata("tryNumberLowerBoundsBeforeUpperBounds.kt")
+            public void testTryNumberLowerBoundsBeforeUpperBounds() throws Exception {
+                doTest("compiler/testData/diagnostics/tests/inference/tryNumberLowerBoundsBeforeUpperBounds.kt");
+            }
+            
             @TestMetadata("typeConstructorMismatch.kt")
             public void testTypeConstructorMismatch() throws Exception {
                 doTest("compiler/testData/diagnostics/tests/inference/typeConstructorMismatch.kt");
